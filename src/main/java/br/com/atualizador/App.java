7695e9bd377ff6e0919c09bc46f3712abdf2d2ef
adailsonacj <adailsonacj@live.com>
2019-05-13 17:24:05 -0300

    melhorias

--- src/main/java/br/com/atualizador/App.java
+++ src/main/java/br/com/atualizador/App.java
@@ -6,39 +6,51 @@ import java.util.List;
 
 public class App {
 
-	// private static String CAMINHO_APLICACAO = "C:\\Users\\adailsonacj\\aaa\\";
-	private static String CAMINHO_APLICACAO = "/home/adailson/aaa/";
+	private static String CAMINHO_APLICACAO = "C:\\Users\\adailsonacj\\aaa\\";
+	private static String NOME_PASTA_APLICACAO = "aaa";
+//	private static String CAMINHO_APLICACAO = "/home/adailson/aaa/";
 
 	public static void main(String[] args) {
+		File aplicacao = new File(CAMINHO_APLICACAO);
 
-		List<String> listaTemp = new ArrayList<String>();
-		if (args != null) {
+		renomearArquivosTemp(listaCaminhos(aplicacao));
 
-			String[] splitTemp = args[0].split("%30");
-			for (int i = 0; i < splitTemp.length; i++) {
-				listaTemp.add(splitTemp[i].replace("%20", " "));
-			}
-			System.out.println(listaTemp);
-			renomearArquivosTemp(listaTemp);
+		excluirArquivosNaoContidosNoServidor(args);
 
-			List<String> listaExclusao = new ArrayList<String>();
-			String[] splitExclusao = args[1].split("%30");
-			for (int i = 0; i < splitExclusao.length; i++) {
-				listaExclusao.add(splitExclusao[i].replace("%20", " "));
-			}
-			System.out.println(listaExclusao);
-			excluirArquivosNaoContidosNoServidor(listaExclusao);
+		String[] splitExclusao = args[0].split("%30");
+		for (String ar : splitExclusao) {
+			System.out.println(ar);
 		}
+		excluirArquivosNaoContidosNoServidor(splitExclusao);
+	}
 
+	public static List<String> listaCaminhos(File dir) {
+		List<String> caminhos = new ArrayList<String>();
+		if (dir == null || dir.listFiles() == null) {
+			return null;
+		}
+		for (File file : dir.listFiles()) {
+			if (file.isFile()) {
+				String[] dados = null;
+				dados = file.getParent().split(NOME_PASTA_APLICACAO, 2);
+				if (dados.length > 1) {
+					caminhos.add(dados[1] + File.separator + file.getName());
+				} else {
+					caminhos.add(File.separator + file.getName());
+				}
+			} else
+				caminhos.addAll(listaCaminhos(file));
+		}
+		return (List<String>) caminhos;
 	}
 
 	public static void renomearArquivosTemp(List<String> arquivosTemporarios) {
-		for (int i = 0; i < arquivosTemporarios.size(); i++) {
-			String nomeTemporario = new File(arquivosTemporarios.get(i)).getName();
+		for (String arq : arquivosTemporarios) {
+			String nomeTemporario = new File(arq).getName();
 			String[] nomeNovo = nomeTemporario.split("_temp_");
 
 			if (nomeNovo.length > 1) {
-				File arquivoTemporario = new File(CAMINHO_APLICACAO + arquivosTemporarios.get(i));
+				File arquivoTemporario = new File(CAMINHO_APLICACAO + arq);
 				File arquivoNovo = new File(arquivoTemporario.getParent() + File.separator + nomeNovo[1]);
 				if (arquivoNovo.exists()) {
 					arquivoNovo.delete();
@@ -48,16 +60,12 @@ public class App {
 		}
 	}
 
-	public static void excluirArquivosNaoContidosNoServidor(List<String> arquivosExlusao) {
-		for (int i = 0; i < arquivosExlusao.size(); i++) {
-			File fi = new File(CAMINHO_APLICACAO + arquivosExlusao.get(i));
+	public static void excluirArquivosNaoContidosNoServidor(String[] arquivosExlusao) {
+		for (String dir : arquivosExlusao) {
+			File fi = new File(CAMINHO_APLICACAO + dir.replace("%20", " "));
 			if (!fi.getName().equals("MD5.txt")) {
-				deletarArquivo(fi);
+				fi.delete();
 			}
 		}
-	}
-
-	public static void deletarArquivo(File file) {
-		file.delete();
 	}
 }
